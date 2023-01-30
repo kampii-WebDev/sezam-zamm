8c26e4a4ccadd4fb12728ff5d77407ca75a81fc0
kacpi <99847508+kampii-WebDev@users.noreply.github.com>
2023-01-30 19:52:30 +0100

    Add files via upload

---
+++ js/delivery-price-pop-up.js
@@ -0,0 +1,9 @@
+function show() {
+	document.getElementById('pop-up').style.opacity = '1';
+	document.getElementById('close').style.opacity = '1';
+};
+
+function hide() {
+	document.getElementById('pop-up').style.opacity = '0';
+	document.getElementById('close').style.opacity = '0';
+};
